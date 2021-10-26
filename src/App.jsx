c26781019d5b39b6450233147fab70ba4f07f3a8
azlaan-asahi <azlaan@asahitechnologies.com>
2021-10-26 20:56:22 +0530

    Layout management based on auth

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,26 @@ import "./App.css";
 import "antd/dist/antd.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
+import { Layout } from "antd";
+
+const { Content, Footer, Header } = Layout;
 function App() {
+  const isAuthenticated = store.getState().auth.isAuthenticated;
   return (
     <Provider store={store}>
-      <AppRoute />
+      {!isAuthenticated ? (
+        <AppRoute />
+      ) : (
+        <Layout>
+          <Header style={{ color: "white !important" }}>
+            MOVIES TICKETING SYSTEM
+          </Header>
+          <Content>
+            <AppRoute />
+          </Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      )}
     </Provider>
   );
 }
